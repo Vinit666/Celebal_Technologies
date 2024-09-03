e8610b90f938d05fa166ff18e38a6a27700a25c9
Vinit Kumawat <128893260+Vinit666@users.noreply.github.com>
2024-09-03 09:01:05 +0530

    Delete TopCompetitors.sql

--- TopCompetitors.sql
+++
@@ -1,8 +0,0 @@
-SELECT c.hacker_id, d.name
-FROM Difficulty a JOIN Challenges b ON a.difficulty_level = b. difficulty_level
-                                                        JOIN Submissions  c ON c.challenge_id = b.challenge_id 
-                                                        JOIN Hackers  d ON d.hacker_id  = c.hacker_id 
-WHERE c.score = a.score
-GROUP BY c.hacker_id, d.name
-HAVING COUNT(c.challenge_id) > 1
-ORDER BY COUNT(c.challenge_id) DESC, c.hacker_id ASC;
