e8578ddd00f28af4ae64cc5d48d94604fa496f5a
Vinit Kumawat <128893260+Vinit666@users.noreply.github.com>
2024-09-03 09:00:51 +0530

    Delete TheReport.sql

--- TheReport.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-CASE
-  WHEN Grade >= 8 THEN Name
-  ELSE NULL
-END AS Name, Grade, Marks 
-FROM Students INNER JOIN Grades 
-ON Students.Marks BETWEEN Grades.Min_Mark AND Grades.Max_Mark 
-ORDER BY Grade DESC, Name ASC, Marks ASC;
